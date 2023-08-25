b02bee7ea068d6696176c4debc4c94372db9b315
jcinert <jcinert@gmail.com>
2023-08-25 10:00:05 +0200

    CCSS CoT experimentation

---
+++ CommonCoreLearningStandardsAI/prompts.py
@@ -0,0 +1,14 @@
+PROMPT_DEFINITION = """What is common core standard {standard} definition?"""
+
+PROMPT_1 = """Given the You need to create an assesment for a student consisting of:\
+ 1. context, 2. free responce question, 3. rubic for evaluation.\
+ Think step by step and provide context, free responce question and the rubic.\
+ Remember the assignmet must be according to CCSS.ELA-LITERACY.W.4.9 standard and use basketball topic."""
+
+
+PROMPT_X = """You need to create an assesment for a student consisting of:\
+ 1. context, 2. free responce question, 3. rubic for evaluation.\
+ Think step by step and provide context, free responce question and the rubic.\
+ Remember the assignmet must be according to CCSS.ELA-LITERACY.W.4.9 standard and use basketball topic."""
+
+PROMPT_RUBRIC = """Create a rubic for the CCSS.ELA-LITERACY.W.4.9 assesment"""
