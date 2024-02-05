afffca89836d90c4a0369b8b8f46ea01ade72aad
jcinert <jcinert@gmail.com>
2024-02-06 00:05:55 +0100

    RAG agent init

--- LLM/RAG/Chat_with_PythonSDK_agent/README.md
+++ LLM/RAG/Chat_with_PythonSDK_agent/README.md
@@ -1,8 +1,35 @@
+# Chat with an agent over a web site
+This is a RAG chat with agent, powered by Azure OpenAI LLMs. You can chat with [Generative AI Python SDK](https://github.com/IBM/).
+Agent can search web as well.
 
-cd C:\Github_Projects\Experiments\CondaEnv
-conda activate ./env
-cd C:\Github_Projects\Experiments\LLM\RAG\Chat_with_PythonSDK_agent
-streamlit run main.py
-pause
+> **Warning:** This is a draft solution, by no means ready for production.
 
+> **Warning 2:** Due to limited version of my Azure OpenAI model (S0 tier), there is a delay in chat response (~ 10s ). this can be solved by upgrading the tier
 
+> **Info:** Standard RAG chat (no agent) version might perform better - a version is here: [RAG chat with web site](https://github.com/jcinert/Experiments/tree/main/LLM/RAG/Chat_with_PythonSDK).
+
+# Modifications possible
+- You can replace the knowledge base (RAG) with __another web site__ - see `config.json`
+- You can replace the Azure OpenAI with __another LLM__ - see `./src/chat-chain.py` (main model) and `./src/vector_db.py` (embedding model)
+
+# How to run Chat - with Conda Environment
+1. Clone/copy this repo
+1. update Auzre Open AI details in `env_template.env` and file path in `config.json`
+3. Open Conda prompt (or VS code)
+4. navigate to your repo environment folder `cd C:\<your path>\CondaEnv`
+5. `conda activate ./env`
+6. navigate to your repo chat folder `cd C:\<your path>\Chat_with_PythonSDK_agent`
+7. Run in conda prompt `streamlit run main.py`
+8. Browser window should open with streamlit chat
+
+Without Conda you need to install all pre-requisites listed in `./CondaEnv/environment.yml`. Then start with step 5.
+
+# References
+Great Repos I drew inspiration from:
+- https://github.com/langchain-ai/streamlit-agent/blob/main/streamlit_agent/basic_memory.py
+
+# TODO Next
+- Add refresh frequency to vector DB
+- Improve istory handling - sometimes the history is not correctly taken into account. forcing the agent to rephrase first should help (or use GPT4)
+- Code snippets and examples are rarely provided. few shot learning might help
+- Use ChatGPT 4
--- LLM/RAG/Chat_with_PythonSDK_agent/main.py
+++ LLM/RAG/Chat_with_PythonSDK_agent/main.py
@@ -1,70 +1,128 @@
 import streamlit as st
-from langchain.memory import ConversationBufferMemory
-from langchain.memory.chat_message_histories import StreamlitChatMessageHistory
-from langchain.agents import AgentType, initialize_agent, load_tools
-from langchain.callbacks import StreamlitCallbackHandler
-import openai, os
-from langchain.chat_models import  AzureChatOpenAI
-from dotenv import load_dotenv, find_dotenv
-
 import os
 import openai
-import bs4
-from langchain import hub
-from langchain.chat_models import  AzureChatOpenAI
-from langchain.document_loaders import WebBaseLoader
-from langchain.embeddings import AzureOpenAIEmbeddings
-from langchain.schema import StrOutputParser
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import Chroma
-# from langchain_core.runnables import RunnablePassthrough
-from langchain.schema.runnable  import RunnablePassthrough
-from ssl_workaround import no_ssl_verification
+# from src.ssl_workaround import no_ssl_verification
 from dotenv import load_dotenv, find_dotenv
 from src.vector_db import VectorDB
+from langchain.agents import initialize_agent, Tool
+from langchain.chains import RetrievalQA
+from langchain_community.chat_models import AzureChatOpenAI
+from langchain_community.tools import DuckDuckGoSearchResults
+from langchain_community.utilities import DuckDuckGoSearchAPIWrapper
+from langchain_community.callbacks import StreamlitCallbackHandler
+from langchain_community.chat_message_histories import StreamlitChatMessageHistory
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory
 
 # ------ INIT ------
 st.set_page_config(page_title="Chat with IBM Generative AI Python SDK", page_icon="🖖")
 st.title("🖖 Chat with IBM Generative AI Python SDK")
 
-@st.cache_resource
+@st.cache_resource # run just once
 def init():
     _ = load_dotenv(find_dotenv()) # read local .env file
     openai.api_key = os.environ['OPENAI_API_KEY']
     openai.api_base = os.environ['OPENAI_API_BASE']
     openai.api_type= os.environ['OPENAI_API_TYPE']
     openai.api_version = os.environ['OPENAI_API_VERSION']
-    print(f'Openai secrets loaded, models: {os.environ["OPENAI_DEPLOYMENT_ID_LLM"]}, {os.environ["OPENAI_DEPLOYMENT_ID_EMBED"]}')
+    print(f'>>> Openai secrets loaded, models: {os.environ["OPENAI_DEPLOYMENT_ID_LLM"]}, {os.environ["OPENAI_DEPLOYMENT_ID_EMBED"]}')
 
-    # Set up memory
-    msgs = StreamlitChatMessageHistory(key="langchain_messages")
-    memory = ConversationBufferMemory(chat_memory=msgs)
-
-    # load vector DB
+    # ------ load vector DB ------
     vector_db = VectorDB()
-    vector_db.create_db_from_file()
-    retriever = vector_db.get_retriever()
-    # retriever test
-    retrieved_docs = retriever.get_relevant_documents(
-        "What are parameters of class Credentials?"
-    )
-    print(f'---Number of documents retrieved: {len(retrieved_docs)}')
-    print(f'---Doc 0 length: {len(retrieved_docs[0].page_content)}')
-    print(f'---Doc 0: {retrieved_docs[0].page_content}')
-    print(f'---Doc 1 length: {len(retrieved_docs[1].page_content)}')
-    print(f'---Doc 1: {retrieved_docs[1].page_content}')
+    try:
+        vector_db.create_db_from_file()
+        print(f'>>> Vector DB loaded from file')
+    except:
+        vector_db.create_db_from_url()
+        print(f'>>> Vector DB loaded from url')
 
-    # create a chat instance
+    retriever = vector_db.get_retriever()
+
+    # ------ create a chat instance ------
     llm = AzureChatOpenAI(azure_deployment=os.environ['OPENAI_DEPLOYMENT_ID_LLM'],
                         temperature=0, 
+                        max_retries=10,
                         streaming=True)
-    tools = load_tools(["ddg-search"])
-    agent = initialize_agent(
-        tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True, handle_parsing_errors=True
+    # ------ retrieval qa chain ------
+    qa = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=retriever
     )
-    return agent, msgs
 
-agent, msgs = init()
+    # ------ load tools ------
+    wrapper = DuckDuckGoSearchAPIWrapper(time="y", max_results=3) # one year search
+    search = DuckDuckGoSearchResults(api_wrapper=wrapper)
+    tools = [
+        Tool(
+            name='Knowledge Base',
+            func=qa.run,
+            description=(
+                'First tool to use when answering questions about LLM, LLM apps, generative AI frameworks and python APIs'
+            )
+        ),
+        Tool(
+            name='Web Search',
+            func=search.run,
+            description=(
+                'Tool to use for generic questions, mainly for recent events and information.'
+            )
+        )
+    ]
+
+    # ------ Set up memory ------
+    msgs = StreamlitChatMessageHistory(key="chat_history")
+    memory = ConversationBufferWindowMemory(
+        memory_key='chat_history',
+        k=5,
+        return_messages=True
+    )
+
+    # ------ create agent ------
+    agent = initialize_agent(
+        agent='chat-conversational-react-description',
+        tools=tools,
+        llm=llm,
+        verbose=True,
+        max_iterations=4,
+        early_stopping_method='generate',
+        memory=memory,
+        handle_parsing_errors=True
+    )
+
+
+    # ------ create custom sys mesage ------
+    # steering agent to use the Knowlege base, but not limiting it to answer any question or use web search
+    sys_msg = """Assistant is a large language model.
+Assistant is designed to be able to assist with a wide range of tasks related to LLMs, machine learning and programming , \
+from answering simple questions to providing in-depth explanations, example code and links to documentation. \
+As a language model, Assistant is able to generate human-like text based on the input it receives, \
+allowing it to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand.
+Assistant can use code spippets and URL links.
+
+Assistant is constantly learning and improving, and its capabilities are constantly evolving. \
+It is able to process and understand large amounts of text, and can use this knowledge to provide accurate \
+and informative responses to a wide range of questions. Additionally, Assistant is able to generate its own text based on the input \
+it receives, allowing it to engage in discussions and provide explanations and descriptions on a wide range of topics.
+
+If the question is related to LLM, LLM apps, generative AI frameworks and python APIs Assistant has to first use the Knowledge Base. \
+If no relevant answer is found in the Knowlege Base, Assistant can use Web Search tool to find an answer on the internet. \
+If the question is not related to LLMs, machine learning and programming Assistant can use Web Search tool to find an answer on the internet.
+
+"""
+
+    new_prompt = agent.agent.create_prompt(
+        system_message=sys_msg,
+        tools=tools
+    )
+    agent.agent.llm_chain.prompt = new_prompt
+
+    print(f'>>> sys. msg: {agent.agent.llm_chain.prompt.messages[0].prompt.template}')
+    print(f'>>> INIT complete - agent created')
+    return agent, msgs, memory
+
+# ------ MAIN ------
+# run init
+agent, msgs, memory = init()
 
 if len(msgs.messages) == 0:
     msgs.add_ai_message("How can I help you?")
@@ -75,14 +133,16 @@ for msg in msgs.messages:
 
 # ------ CHAT ------
 if prompt := st.chat_input():
+    # write prompt to UI
     st.chat_message("user").write(prompt)
     msgs.add_user_message(prompt)
 
     with st.chat_message("assistant"):
         st_callback = StreamlitCallbackHandler(st.container())
-        response = agent.run(prompt, callbacks=[st_callback])
+        response = agent.run(input=prompt, callbacks=[st_callback])
         st.write(response)
+        print(f'>>> hist: {memory.chat_memory.messages}')
 
-    # st.chat_message("ai").write(response)
+    # write responce to UI
+    st.chat_message("ai").write(response)
     msgs.add_ai_message(response)
-    print(msgs.messages)
